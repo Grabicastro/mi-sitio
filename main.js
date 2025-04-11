29ac58e71882a2f0eb85484ef50dbd88d9943d6d
GRABIELA CASTRO <99205576+Grabicastro@users.noreply.github.com>
2025-04-11 10:40:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const text = "Bienvenido a mi espacio donde la creatividad, tecnología y la espiritualidad se fusionan para inspirar y conectar.";
+let index = 0;
+const speed = 50; // velocidad de escritura en ms
 
+function typeEffect() {
+  if (index < text.length) {
+    document.getElementById("typed-text").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(typeEffect, speed);
+  }
+}
+
+window.onload = typeEffect;
